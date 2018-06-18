7f0197fa3dca872f1799c9b54a00c52d274d243d
m0rtis <mail@m0rtis.ru>
2018-06-18 17:49:50 +0300

    Refactored (simplified) Container::retrieve method

--- src/Container.php
+++ src/Container.php
@@ -35,8 +35,7 @@ class Container implements ContainerInterface, \ArrayAccess, \Iterator, \Countab
     {
         $this->data = $data;
 
-        $config = $data['config'][ContainerInterface::class] ?? [];
-        $this->returnShared = (bool)($config['return_shared'] ?? true);
+        $this->returnShared = (bool)($data['config'][ContainerInterface::class]['return_shared'] ?? true);
     }
 
     /**
@@ -284,14 +283,9 @@ class Container implements ContainerInterface, \ArrayAccess, \Iterator, \Countab
         $item = $this->data[$id] ?? $id;
         try {
             if (\is_string($item)) {
-                $resolved = $this->resolve($item);
-                if ($this->isCallable($resolved)
-                    && false !== \stripos($item, 'factory')
-                ) {
-                    $resolved = $this->call($resolved);
-                }
-                $item = $resolved;
-            } elseif (\is_callable($item)) {
+                $item = $this->resolve($item);
+            }
+            if ($this->isCallable($item)) {
                 $item = $this->call($item);
             }
         } catch (\Exception $e) {
